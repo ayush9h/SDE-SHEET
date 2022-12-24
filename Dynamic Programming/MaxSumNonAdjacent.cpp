0d793046b42b32fe4dad0c98548238104fff1383
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2022-12-24 19:32:58 +0530

    Update MaxSumNonAdjacent.cpp

--- Dynamic Programming/MaxSumNonAdjacent.cpp
+++ Dynamic Programming/MaxSumNonAdjacent.cpp
@@ -2,13 +2,15 @@
 int solve(int ind,vector<int>&nums,vector<int>&dp)
 {
     if(ind==0)
+        return nums[ind];
+    if(ind<0)
         return 0;
     
+    if(dp[ind]!=-1)
+        return dp[ind];
     int pick = nums[ind] + solve(ind-2,nums,dp);
     int notPick = 0 + solve(ind-1,nums,dp);
-    
-    
-    return max(pick,notPick)
+    return dp[ind] = max(pick,notPick);
 }
 int maximumNonAdjacentSum(vector<int> &nums){
     int n = nums.size();
